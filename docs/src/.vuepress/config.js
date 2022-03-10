dec593afa309483b4e6b705bba816608fb3fe2b8
jingle-baba <umesh@anattadesign.com>
2022-03-10 22:32:27 +0530

    Favicon fixed on github pages

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -20,6 +20,7 @@ module.exports = {
     ['meta', { name: 'theme-color', content: '#ff322e' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
   ],
 
   /**
